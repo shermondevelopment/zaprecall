fb3e9c5642d9932102208a0b77fea03f7fe009e0
Victor Shermon <victor804.gt@gmail.com>
2022-03-20 10:50:44 -0300

    removendo bug do formulario de decks

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,24 +10,26 @@ const Home = ({ stateHome, deckOption }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(deck)
-    deck.length !== 0 && deckOption(deck)
+    deck.length !== 0 && deckOption(!deck || 'javascript')
     step === 1 ? setStep(2) : stateHome(false)
   }
 
-
+ 
   return (
     <section className="home">
       <img src="/assets/img/lightning.svg" alt="zap" className="home-logo" />
       <h1 className="title-home">ZapRecall</h1>
       <form className="form" onSubmit={(e) => handleSubmit(e)}>
+        
+        {step === 2 && 
+            <select name="tech" className={`input-meta ${step === 1 && 'none'}`} onChange={(e) => setDeck(e.currentTarget.value)} required>
+                <option value="">Escolha seu Deck</option>
+                <option value="javascript">Javascript</option>
+                <option value="html5">HTML5</option>
+                <option value="css3">CSS3</option>
+            </select>
+        }
 
-          <select className={`input-meta ${step === 1 && 'none'}`} onChange={(e) => setDeck(e.currentTarget.value)}>
-            <option value="javascript">Escolha seu Deck</option>
-            <option value="javascript">Javascript</option>
-            <option value="html5">HTML5</option>
-            <option value="css3">CSS3</option>
-          </select>
         
         {step === 1 && 
           <input type="text" placeholder="Digite sua meta de zaps... Ex: 1, 2" className="input-meta" required pattern='\d'  />
